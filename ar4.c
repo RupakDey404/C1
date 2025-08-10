58adb867a53454eeba55bf8a09ed95385f1d5e04
RupakDey404 <officialrupak670@gmail.com>
2025-08-10 20:35:46 +0530

    Add files via upload

---
+++ ar4.c
@@ -0,0 +1,21 @@
+//ROTATE AN ARRAY LEFT TO RIGHT BY K th POSITION.
+#include<stdio.h>
+int main(){
+    int arr[]={10,20,30,40,50,60};
+    int arr2[100];
+    int n=sizeof(arr)/4;
+    int x;
+    
+    printf("ENTER THE INDEX:");
+    
+    scanf("%d",&x);
+    x=x % n ;
+    for(int i=0;i<n;i++){
+        arr2[i]=arr[(x+i) % n];
+    }
+    printf("ARRAY AFTER ROTATION:\n");
+    for(int i=0;i<n;i++){
+        printf("%d ",arr2[i]);
+    }
+    printf("\n");
+}
