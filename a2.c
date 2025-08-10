887c646f6da485f74a403f5db1c7bd806410b477
RupakDey404 <officialrupak670@gmail.com>
2025-08-10 12:05:32 +0530

    Update a2.c

--- a2.c
+++ a2.c
@@ -1,4 +1,4 @@
-//SECOND LARGEST ELEMENT IN AN ARRAY
+//SECOND LARGEST ELEMENT IN AN ARRAY.
 #include<stdio.h>
 int main(){
     int arr[]={23,9,-1,-45,90,10};
@@ -22,3 +22,4 @@ int main(){
 
 
 }
+
