8de9a7c06ad9695c5723bdfa9920ba222819ef92
RupakDey404 <officialrupak670@gmail.com>
2025-08-10 21:39:10 +0530

    Add files via upload

---
+++ ar4.py
@@ -0,0 +1,7 @@
+#ROTATE AN ARRAY  TO THR LEFT  BY X th POSTIONS
+import numpy as np
+arr=np.array([12,34,4,56,78])
+x=int(input("ENTER INDEX:"))
+x=x % len(arr)
+rotated_array=np.roll(arr,-x)#"-VE" for left side rotation
+print(rotated_array)
