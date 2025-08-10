bfefb9a78206efa2a8385f221d6a3bf065651253
RupakDey404 <officialrupak670@gmail.com>
2025-08-10 21:52:46 +0530

    Update ar4.py

--- ar4.py
+++ ar4.py
@@ -3,6 +3,7 @@ import numpy as np
 arr=np.array([12,34,4,56,78])
 x=int(input("ENTER INDEX:"))
 x=x % len(arr)
-rotated_array=np.roll(arr,-x)#"-VE" for left side rotation
+rotated_array=np.roll(arr,-x) #"-VE" for left side rotation
 print(rotated_array)
+
 
